0f397c7798eb354ea640e6179a01c5dce92ddf51
evgeniyamiller <evgeniya_hvorostova@mail.ru>
2022-11-23 01:12:51 +0400

    LIST PRODUCT

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import product.Product;
+import listProducts.listProducts;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product tomato = new Product("Томат", 130, 4.0);
+        Product potato = new Product("Картофель", 55, 4.5);
+        Product carrot = new Product("Морковь", 60, 1.5);
+        Product cabbage = new Product("Капуста", 99, 1.3);
+        Product onion = new Product("Лук", 43, 2.2);
+        Product meat = new Product("Мясо", 800, 4.0);
+        Product bread = new Product("Хлеб", 67, 1.0);
+
+        listProducts listProducts = new listProducts();
+
+
+        listProducts.addProduct(tomato);
+        listProducts.addProduct(potato);
+        listProducts.addProduct(carrot);
+        listProducts.addProduct(cabbage);
+        listProducts.addProduct(onion);
+        listProducts.addProduct(meat);
+        listProducts.addProduct(bread);
+        listProducts.showInfo();
+
+        listProducts.removeProduct(potato);
+        listProducts.showInfo();
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ src/listProducts/listProducts.java
@@ -0,0 +1,43 @@
+package listProducts;
+
+import product.Product;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class listProducts {
+    public static Set<Product> set = new HashSet<>();
+
+    public listProducts() {
+    }
+
+
+    public void addProduct(Product product) {
+        if (!set.contains(product)) {
+            set.add(product);
+        } else {
+            throw new RuntimeException("Продукт " + product.getName()+ " в корзине!");
+        }
+    }
+
+    public void removeProduct(Product product){
+        Iterator<Product> productIterator = this.set.iterator();
+        while (productIterator.hasNext()){
+            if(productIterator.next().getName().equals(product.getName())) {
+                productIterator.remove();
+                break;
+            }
+        }
+    }
+
+    public void showInfo (){
+        System.out.println("Список продуктов");
+        for (Product product:set) {
+            System.out.println(product.getName() + " " + product.getQuantity() + " кг, " + product.getPrice() + " руб.");
+        }
+
+    }
+
+
+}
